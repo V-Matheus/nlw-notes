c424fb78b12613d3921472b9373c567d1065300a
Victor Matheus <victormatheus507@gmail.com>
2024-02-06 14:29:08 -0300

    add modal

--- src/Components/NoteCard.tsx
+++ src/Components/NoteCard.tsx
@@ -20,7 +20,7 @@ const NoteCard = ({ note }: NoteCardProps) => {
 
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-        <Dialog.Content className='z-10 bg-white absolute'>Oi</Dialog.Content>
+        <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none'>Oi</Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
   )
