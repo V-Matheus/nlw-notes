e0f2062b42ea4d1e9027d58cec99c4598333dc0b
Victor Matheus <victormatheus507@gmail.com>
2024-02-06 14:23:49 -0300

    add biblioteca radix

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,25 @@ import NewNoteCard from './Components/NewNoteCard'
 import NoteCard from './Components/NoteCard'
 import logo from './assets/Logo.svg'
 
-function App() {
-  return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
-      <img src={logo} alt="nlw-expert" />
-      <form className='w-full mt-6'>
-        <input placeholder='Busque em suas notas...' type="text" name="" id="" className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' />
-      </form>
+  function App() {
+    return (
+      <div className='mx-auto max-w-6xl my-12 space-y-6'>
+        <img src={logo} alt="nlw-expert" />
+        <form className='w-full mt-6'>
+          <input placeholder='Busque em suas notas...' type="text" name="" id="" className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' />
+        </form>
 
-      <div className='h-px bg-slate-700' />
+        <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 p-4 gap-6 auto-rows-[250px] overflow-hidden'>
-        <NewNoteCard />
-        <NoteCard date={new Date()} content='Hello Word' />
-        <NoteCard date={new Date()} content='teste' />
+        <div className='grid grid-cols-3 p-4 gap-6 auto-rows-[250px] overflow-hidden'>
+          <NewNoteCard />
+          <NoteCard note={{
+            date: new Date(),
+            content: 'Hello Word'
+          }} />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 
 export default App
--- src/Components/NoteCard.tsx
+++ src/Components/NoteCard.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
 
 interface NoteCardProps {
-  date: Date,
-  content: string
+  note: {
+    date: Date,
+    content: string
+  }
 }
 
-const NoteCard = (props: NoteCardProps) => {
+const NoteCard = ({ note }: NoteCardProps) => {
   return (
-    <button className='rounded-md text-left flex flex-col outline-none bg-slate-800 p-5 gap-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400'>
-      <span className='text-sm font-medium text-slate-200'>{props.date.toISOString()}</span>
-      <p className='text-sm leading-6 text-slate-300'>{props.content}</p>
+    <Dialog.Root>
+      <Dialog.Trigger className='rounded-md text-left flex flex-col outline-none bg-slate-800 p-5 gap-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400'>
+        <span className='text-sm font-medium text-slate-200'>{note.date.toISOString()}</span>
+        <p className='text-sm leading-6 text-slate-300'>{note.content}</p>
 
-      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-    </button>
+        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/50' />
+        <Dialog.Content className='z-10 bg-white absolute'>Oi</Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
 
