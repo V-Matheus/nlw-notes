fa0371b8cb76fd09278ed7d35998546fc100a07f
Victor Matheus <victormatheus507@gmail.com>
2024-02-06 09:42:10 -0300

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import logo from './assets/Logo.svg'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <img src={logo} alt="" />
     </>
   )
 }
